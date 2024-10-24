db9f5119646861b8af8a36d7ae0cffc5098ac952
sudesh.tnbt <sudeshacharya110@gmail.com>
2024-10-24 23:04:59 +0545

    Created the object Dinosaur.

--- dino.py
+++ dino.py
@@ -7,6 +7,14 @@ width = 1100
 height = 600
 screen = py.display.set_mode((width, height))
 
+duck = [py.image.load(os.path.join("Assets/Dino", "DinoDuck1.png")),
+        py.image.load(os.path.join("Assets/Dino", "DinoDuck2.png"))]
+
+run = [py.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
+       py.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
+
+jump = [py.image.load(os.path.join("Assets/Dino", "DinoJump.png"))]
+
 variables = [py.image.load(os.path.join("Assets/Bird", "Bird1.png")),
              py.image.load(os.path.join("Assets/Bird", "Bird2.png")),
              py.image.load(os.path.join("Assets/Cactus", "LargeCactus1.png")),
@@ -16,11 +24,8 @@ variables = [py.image.load(os.path.join("Assets/Bird", "Bird1.png")),
              py.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
              py.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png")),
              py.image.load(os.path.join("Assets/Dino", "DinoDead.png")),
-             py.image.load(os.path.join("Assets/Dino", "DinoDuck1.png")),
-             py.image.load(os.path.join("Assets/Dino", "DinoDuck2.png")),
-             py.image.load(os.path.join("Assets/Dino", "DinoJump.png")),
-             py.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
-             py.image.load(os.path.join("Assets/Dino", "DinoRun2.png")),
+
+
              py.image.load(os.path.join("Assets/Dino", "DinoStart.png")),
              py.image.load(os.path.join("Assets/Other", "Cloud.png")),
              py.image.load(os.path.join("Assets/Other", "GameOver.png")),
@@ -28,7 +33,24 @@ variables = [py.image.load(os.path.join("Assets/Bird", "Bird1.png")),
              py.image.load(os.path.join("Assets/Other", "Track.png"))]
 
 class Dinosaur:
-    pass
+    x = 80
+    y = 310
+
+    def __init__(self):
+        self.duck_img = duck
+        self.run_img = run
+        self.jump_img = jump
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x
+        self.dino_rect.y = self.y
+
 
 def main():
     run = True
